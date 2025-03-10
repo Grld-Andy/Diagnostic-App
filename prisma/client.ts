908281dfa6a73268cb4d980be578873e1b326e6f
Andy Ansong <andyansong82@gmail.com>
2025-03-10 08:14:33 +0000

    update schema

--- prisma/client.ts
+++ prisma/client.ts
@@ -5,13 +5,6 @@ declare global {
 }
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+prisma = new PrismaClient();
 
 export default prisma;
