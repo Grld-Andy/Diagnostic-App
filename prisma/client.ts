31d0f2a05ff50ab86a2f35a00154e0678a4f6a78
Andy Ansong <andyansong82@gmail.com>
2025-03-10 08:46:30 +0000

    update schema

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,6 +4,17 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 let prisma: PrismaClient;
-prisma = new PrismaClient();
+
+console.log("node", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  console.log("here");
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    console.log("sdfgwerw");
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
